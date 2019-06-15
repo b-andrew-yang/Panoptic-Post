c572f4e2fc389b747b985d78470444027f119d73
KibaeKim <kibaekim@uvic.ca>
2019-06-15 14:02:52 -0700

    Got JSON data in resources/scripts/sketch.js

---
+++ resources/scripts/sketch.js
@@ -0,0 +1,21 @@
+document.write("Hello World! <br>");
+let data;
+var url = "https://newsapi.org/v2/everything?q=bitcoin&from=2019-05-15&sortBy=publishedAt&apiKey=f84cbefa09be416fad9c35bed0246247";
+
+
+function setup() {
+	createCanvas(200,200);
+	loadJSON(url, gotData);
+	document.write("Goodbye world!<br>");
+}
+
+function gotData(data){
+	document.write("Starting to write JSON<br>");
+	console.log(data);
+	document.write(data["articles"][0]["author"]);
+}
+
+function draw() {
+  // put drawing code here
+}
+
