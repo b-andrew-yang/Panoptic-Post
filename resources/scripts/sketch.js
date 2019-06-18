df20abec03da2745c8e78cabb647816f2752bcf3
KibaeKim <kibaekim@uvic.ca>
2019-06-18 11:44:02 -0700

    Got onclick to work

--- resources/scripts/sketch.js
+++ resources/scripts/sketch.js
@@ -1,4 +1,4 @@
-let data;
+let globalData;
 var url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=f84cbefa09be416fad9c35bed0246247";
 
 
@@ -10,11 +10,23 @@ function setup() {
 
 function gotData(data){
 	// document.write("Starting to write JSON<br>");
+	globalData = data;
 	console.log(data);
 	// document.write(data["articles"][0]["author"]);
 }
 
+function myFunction() {
+	// document.getElementById("demo").innerHTML = globalData["articles"][0]["author"];
+	var author = globalData["articles"][0]["author"].toString();
+	var source = globalData["articles"][0]["source"]["name"];
+	var content = globalData["articles"][0]["content"];
+	var title = globalData["articles"][0]["title"];
+
+	var output = title.concat("<br>", author, "<br>", source, "<br>", content, "<br>");
+	document.getElementById("demo").innerHTML = output;
+}
+
 function draw() {
-  // put drawing code here
+	// put drawing code here
 }
 
