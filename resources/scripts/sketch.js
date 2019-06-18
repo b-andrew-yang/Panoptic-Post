8ff6b97edfa14ae1b5498a4f6c3d0fe2774dd1ef
KibaeKim <kibaekim@uvic.ca>
2019-06-18 11:01:51 -0700

    added sketch.js to index.html

--- resources/scripts/sketch.js
+++ resources/scripts/sketch.js
@@ -1,18 +1,17 @@
-document.write("Hello World! <br>");
 let data;
-var url = "https://newsapi.org/v2/everything?q=bitcoin&from=2019-05-17&sortBy=publishedAt&apiKey=f84cbefa09be416fad9c35bed0246247";
+var url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=f84cbefa09be416fad9c35bed0246247";
 
 
 function setup() {
 	createCanvas(200,200);
 	loadJSON(url, gotData);
-	document.write("Goodbye world!<br>");
+	// document.write("Goodbye world!<br>");
 }
 
 function gotData(data){
-	document.write("Starting to write JSON<br>");
+	// document.write("Starting to write JSON<br>");
 	console.log(data);
-	document.write(data["articles"][0]["author"]);
+	// document.write(data["articles"][0]["author"]);
 }
 
 function draw() {
